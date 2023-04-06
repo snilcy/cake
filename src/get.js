d2b1427b8041aded75c379033ea02e38f6e10851
Johnny Rocket <0.snilcy@gmail.com>
2023-04-06 23:26:56 +0300

    fix(get): universal constructor name

--- src/get.js
+++ src/get.js
@@ -1,3 +1,1 @@
-import { isObject } from './type.js'
-
-export const getConstructorName = (el) => isObject ? el.constructor.name : undefined
+export const getConstructorName = (el) => el?.constructor?.name
