b22ddf965a6d41406b22bad4b46ba90cdac3d6f5
Johnny Rocket <0.snilcy@gmail.com>
2023-04-09 00:13:00 +0300

    fix(type): fix isObject for arrays

--- src/type.js
+++ src/type.js
@@ -1,6 +1,9 @@
 export const isNull = (el) => el === null
-export const isObject = (el) => typeof el === 'object' && !isNull(el)
 export const isArray = (el) => Array.isArray(el)
+export const isObject = (el) =>
+  typeof el === 'object' &&
+    !isNull(el) &&
+    !isArray(el)
 export const isFunction = (el) => typeof el === 'function'
 export const isError = (el) => el instanceof Error
 export const isUndefined = (el) => typeof el === 'undefined'
