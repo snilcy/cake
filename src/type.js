2708bc7d4d8ed8e774923c4013882a3662455ef1
Johnny Rocket <0.snilcy@gmail.com>
2023-04-08 23:25:12 +0300

    feat(type): add isUndefined

--- src/type.js
+++ src/type.js
@@ -3,3 +3,4 @@ export const isObject = (el) => typeof el === 'object' && !isNull(el)
 export const isArray = (el) => Array.isArray(el)
 export const isFunction = (el) => typeof el === 'function'
 export const isError = (el) => el instanceof Error
+export const isUndefined = (el) => typeof el === 'undefined'
