bd8b6b87191178a1df7adf86c0fd742ea56d26b4
Johnny Rocket <0.snilcy@gmail.com>
2023-06-03 18:08:52 +0300

    feat(build): update

--- build/type.d.ts
+++ build/type.d.ts
@@ -1,6 +1,6 @@
 export declare const isString: (el: any) => el is string;
 export declare const isNull: (el: any) => el is null;
-export declare const isArray: (el: any) => boolean;
+export declare const isArray: (el: any) => el is any[];
 export declare const isObject: (el: any) => el is object;
 export declare const isFunction: (el: any) => el is Function;
 export declare const isError: (el: any) => el is Error;
