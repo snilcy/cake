7e370663ee9ca14c9112808d5f599f03f77cec8e
Johnny Rocket <0.snilcy@gmail.com>
2023-12-09 20:40:04 +0300

    feat: update

---
+++ src/dom.ts
@@ -0,0 +1,1 @@
+export const isTextNode = (node: any): node is Text => node instanceof Text
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './ts/utils'
 
 export * from './type'
+export * from './dom'
 export * from './class'
 export * from './object'
 export * from './array'
