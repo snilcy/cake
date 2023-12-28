3ecccc148b38be3cff02a7c8f68e4d57d9009bcd
Johnny Rocket <0.snilcy@gmail.com>
2023-12-28 14:11:50 +0300

    feat: update

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -2,7 +2,9 @@ import type { DeepPartial, ValuesType } from 'utility-types'
 
 export type IDeepPartial<T> = DeepPartial<T>
 
-export type IObject = Record<string, any>
+export type IObject<T = any> = Record<string, T>
+
+export type IBooleanValue = IObject<boolean>
 
 export type IValuesType<
   T extends ArrayLike<any> | readonly any[] | Record<any, any>,
