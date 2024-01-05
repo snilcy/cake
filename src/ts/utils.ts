d70a427f453756ccba01cc7182b9a5f228b4f601
Johnny Rocket <0.snilcy@gmail.com>
2024-01-06 01:44:17 +0300

    feat: update

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -2,10 +2,15 @@ import type { DeepPartial, ValuesType } from 'utility-types'
 
 export type IDeepPartial<T> = DeepPartial<T>
 
-export type IObject<T = any> = Record<string, T>
+export type IObject<
+  V = any,
+  K extends number | string | symbol = string,
+> = Record<K, V>
 
 export type IBooleanValue = IObject<boolean>
 
 export type IValuesType<
   T extends ArrayLike<any> | readonly any[] | Record<any, any>,
 > = ValuesType<T>
+
+export type IWriteable<T> = { -readonly [P in keyof T]: T[P] }
